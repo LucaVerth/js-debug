70491bf3daf0b020b7456bd5a50811eae34b3ec0
LucaVerth <luca.verthuy@gmail.com>
2021-12-09 17:45:33 +0100

    Exercise done // working on Bonus milestone

--- bundle-03/main.js
+++ bundle-03/main.js
@@ -85,10 +85,78 @@ console.log(dieselCars);
 console.log('Tutte le altre auto');
 console.log(otherCars);
 
-// 1- il codice dovrebbe
-// 2- questo codice presenta un errore di sintassi. ()
-// 3- questo codice presenta un errore di logica.
+// 1- il codice in base ad una array di oggetti, dovrebbe stabilire il tipo di alimentazione e separarli nei rispettivi array
+// 2- questo codice presenta un errore di sintassi. ( mancanza di virgola all'interno dell'array di oggetti, errore di sintassi arrow function)
+// 3- questo codice presenta un errore di logica. ( vedere correzione, mancanza di .toLowerCase per leggere string con lettere maiuscole)
 // soluzione:
 /* 
+const cars = [
+  {
+      manufacturer: 'Ford',
+      model: 'Fiesta',
+      type: 'diesel'
+  },
+  {
+      manufacturer: 'Audi',
+      model: 'A1',
+      type: 'benzina'
+  },
+  {
+      manufacturer: 'Volkswagen',
+      model: 'Golf',
+      type: 'Benzina'
+  },
+  {
+      manufacturer: 'Fiat',
+      model: 'Panda',
+      type: 'metano'
+  },
+  {
+      manufacturer: 'Fiat',
+      model: 'Multipla',
+      type: 'GPL'
+  },
+  {
+      manufacturer: 'Tesla',
+      model: 'Model 3',
+      type: 'elettrico'
+  },
+  {
+      manufacturer: 'Volkswagen',
+      model: 'Polo',
+      type: 'benzina'
+  },
+  {
+      manufacturer: 'Ford',
+      model: 'Kuga',
+      type: 'Diesel'
+  },
+  {
+      manufacturer: 'Seat',
+      model: 'Ibiza',
+      type: 'metano'
+  },
+  {
+      manufacturer: 'Audi',
+      model: 'R8',
+      type: 'Benzina'
+  },
+];
 
+const gasolineCars = cars.filter(auto => auto.type.toLowerCase() === 'benzina');
+
+const dieselCars = cars.filter(auto => auto.type.toLowerCase() === 'diesel');
+
+const otherCars = cars.filter(auto =>  auto.type.toLowerCase() !== 'benzina' && auto.type.toLowerCase() !== 'diesel');
+
+console.log('Auto a benzina');
+console.log('*******************************');
+console.log(gasolineCars);
+
+console.log('Auto a diesel');
+console.log('*******************************');
+console.log(dieselCars);
+
+console.log('Tutte le altre auto');
+console.log(otherCars);
 */
